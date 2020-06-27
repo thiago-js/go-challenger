d61af513578f94668d3a1462a126f69d7acaff18
thiago-js <thiago.jsimao.me@gmail.com>
2020-06-27 16:32:41 -0300

    feat: the minimal structure to program go

---
+++ 1-minimal-structure.go
@@ -0,0 +1,8 @@
+// all programs need this `package main` for working
+// because this package is the entry-point to program go
+package main
+
+// this execution to main *Entry point*
+func main() {
+
+}
